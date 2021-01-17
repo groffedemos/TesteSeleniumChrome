b8657eb9b29c8f4b3d25e8e52175a7f16fac0316
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-16 22:20:01 -0300

    Testes
    Testes

---
+++ TesteSelenium/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace TesteSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando o uso de Selenium..");
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+            //chromeOptions.UnhandledPromptBehavior = UnhandledPromptBehavior.Dismiss;
+
+            ChromeDriver driver;
+
+            if (Environment.OSVersion.VersionString.ToLower().Contains("windows"))
+                driver = new ChromeDriver("D:\\Selenium\\Chrome\\", chromeOptions);
+            else
+                driver = new ChromeDriver(chromeOptions);
+
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+            //driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(30);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+
+            var siteTestes = Environment.GetEnvironmentVariable("SiteTestes");
+            driver.Navigate().GoToUrl("https://github.com/");
+
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine($"Resultado: {driver.Title}");
+
+            //Close the browser
+            driver.Quit();
+        }
+    }
+}
