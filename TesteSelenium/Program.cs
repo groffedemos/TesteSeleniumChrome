4953b43737e3a18954593cf025a02365b140f655
Renato Groffe <renato.groffe@yahoo.com.br>
2021-01-17 01:28:51 -0300

    Update Program.cs

--- TesteSelenium/Program.cs
+++ TesteSelenium/Program.cs
@@ -15,10 +15,10 @@ namespace TesteSelenium
             var chromeOptions = new ChromeOptions();
             chromeOptions.AddArgument("--headless");
 
-            var service = ChromeDriverService.CreateDefaultService();
+            /*var service = ChromeDriverService.CreateDefaultService();
             service.SuppressInitialDiagnosticInformation = true;
             service.EnableVerboseLogging = false;
-            service.LogPath = "selenium.log";
+            service.LogPath = "selenium.log";*/
 
             chromeOptions.SetLoggingPreference(LogType.Browser, LogLevel.Off);
             chromeOptions.SetLoggingPreference(LogType.Driver, LogLevel.Off);
@@ -28,21 +28,22 @@ namespace TesteSelenium
             if (Environment.OSVersion.VersionString.ToLower().Contains("windows"))
                 driver = new ChromeDriver("D:\\Selenium\\Chrome\\", chromeOptions);
             else
-                driver = new ChromeDriver(service, chromeOptions);
+                //driver = new ChromeDriver(service, chromeOptions);
+                driver = new ChromeDriver(chromeOptions);
 
 
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
-            driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(60);
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+            //driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(60);
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
             
             System.Threading.Thread.Sleep(3000);
 
             //var siteTestes = Environment.GetEnvironmentVariable("SiteTestes");
-            //driver.Navigate().GoToUrl("https://www.uol.com.br");
+            driver.Navigate().GoToUrl("https://www.uol.com.br");
             //driver.Navigate().GoToUrl("https://anp-imagemnasa.azurewebsites.net/");
-            driver.Navigate().GoToUrl("https://github.com");
+            //driver.Navigate().GoToUrl("https://github.com");
 
-            //System.Threading.Thread.Sleep(3000);
+            System.Threading.Thread.Sleep(3000);
 
             Console.WriteLine($"Resultado: {driver.Title}");
 
